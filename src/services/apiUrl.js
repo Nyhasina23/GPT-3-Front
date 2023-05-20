481cdc373d49aa2125907339038810de79db7369
Nyhasina23 <nyhasina.finaritra@gmail.com>
2023-05-20 10:59:58 +0200

    fix api url

--- src/services/apiUrl.js
+++ src/services/apiUrl.js
@@ -1,3 +1,3 @@
-// export const apiURL = "http://51.254.39.222:3005"
-export const apiURL = "http://localhost:3005"
+export const apiURL = "http://51.254.39.222:3005"
+// export const apiURL = "http://localhost:3005"
 export const fileServerAPI = "http://51.254.39.222:3001"
