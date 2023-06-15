99b01586ec9d497cabd5028e0d747874499c404a
Nyhasina23 <nyhasina.finaritra@gmail.com>
2023-06-15 20:49:48 +0200

    update url

--- src/services/apiUrl.js
+++ src/services/apiUrl.js
@@ -1,4 +1,4 @@
-// export const apiURL = "http://51.254.39.222:3005"
-export const apiURL = "http://localhost:3005";
-// export const fileServerAPI = "http://51.254.39.222:3002"
-export const fileServerAPI = "http://localhost:3002";
+export const apiURL = "http://51.254.39.222:3005";
+// export const apiURL = "http://localhost:3005";
+export const fileServerAPI = "http://51.254.39.222:3002"
+// export const fileServerAPI = "http://localhost:3002";
