e197470ef00d620f5c7ece99652439a0e29a5626
Nyhasina23 <nyhasina.finaritra@gmail.com>
2023-07-21 11:59:34 +0200

    update api

--- src/services/apiUrl.js
+++ src/services/apiUrl.js
@@ -1,4 +1,4 @@
-// export const apiURL = "https://api.winepal.fr";
-export const apiURL = "http://localhost:3005";
-// export const fileServerAPI = "https://file.winepal.fr";
-export const fileServerAPI = "http://localhost:3002";
+export const apiURL = "https://api.winepal.fr";
+// export const apiURL = "http://localhost:3005";
+export const fileServerAPI = "https://file.winepal.fr";
+// export const fileServerAPI = "http://localhost:3002";
